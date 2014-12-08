309b003f08ad8387e24792c6cc853e223b8b1de4
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-12-08 18:51:28 +0000

    updated to jquery 2.1.1 to fix slideshow bug

--- public/scripts/slideshow.js
+++ public/scripts/slideshow.js
@@ -1,22 +1,24 @@
-﻿
-$(function(){
+var fadeFunc;
+$(document).ready(function() {
     $('#fadein img:gt(0), #fadein2 img:gt(0), #fadein3 img:gt(0), #fadein4 img:gt(0), #mainWindow img:gt(0)').hide();
-	setInterval(function(){
-	    $('#fadein :first-child').fadeOut(000)
-		    .next('img').fadeIn(1000)
-		    .end().appendTo('#fadein');
-	    $('#fadein2 :first-child').fadeOut(000)
-            .next('img').fadeIn(1000)
-            .end().appendTo('#fadein2');
-	    $('#fadein3 :first-child').fadeOut(000)
-            .next('img').fadeIn(1000)
-            .end().appendTo('#fadein3');
-	    $('#fadein4 :first-child').fadeOut(000)
-            .next('img').fadeIn(1000)
-            .end().appendTo('#fadein4');
-	    $('#mainWindow :first-child').fadeOut(000)
-            .next('img').fadeIn(1000)
-            .end().appendTo('#mainWindow');
-	}, 2000);
+    fadeFunc = setInterval(fadePictures, 2000);
 });
 
+function fadePictures() {
+    console.log("Fading in next set");
+    $('#fadein :first-child').fadeOut(0)
+        .next('img').fadeIn(1000)
+        .end().appendTo('#fadein');
+    $('#fadein2 :first-child').fadeOut(0)
+        .next('img').fadeIn(1000)
+        .end().appendTo('#fadein2');
+    $('#fadein3 :first-child').fadeOut(0)
+        .next('img').fadeIn(1000)
+        .end().appendTo('#fadein3');
+    $('#fadein4 :first-child').fadeOut(0)
+        .next('img').fadeIn(1000)
+        .end().appendTo('#fadein4');
+    $('#mainWindow :first-child').fadeOut(0)
+        .next('img').fadeIn(1000)
+        .end().appendTo('#mainWindow');
+}
